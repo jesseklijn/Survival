fa301d2b2041654dba62670ac7b7943cd5665c2f
jesseklijn <jesseklijn@hotmail.com>
2017-05-31 00:10:47 +0200

    Water Projection Caustics

---
+++ Survival Island/Assets/Project/Scripts/Water/Gifcycler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gifcycler : MonoBehaviour
+{
+
+    public Texture2D[] GifImages;
+    public Material toChange;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(RotateTowardsClosestWayPoint(0));
+    }
+
+    IEnumerator RotateTowardsClosestWayPoint(int i)
+    {
+        if (i == GifImages.Length)
+        {
+            i = 0;
+        }
+        toChange.SetTexture("_ShadowTex", GifImages[i]);
+        i++;
+        yield return 0;
+        StartCoroutine(RotateTowardsClosestWayPoint(i));
+    }
+}
